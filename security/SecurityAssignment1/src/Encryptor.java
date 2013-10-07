ffe3fef88f4417fa95af3279c2fdc314b66e0408
rychvald <marcel.stolz@students.unibe.ch>
2013-10-07 02:01:59 +0200

    Encryiption seems to work, however we have a problem handling files / folders with spaces in their names!

--- security/SecurityAssignment1/src/Encryptor.java
+++ security/SecurityAssignment1/src/Encryptor.java
@@ -1,22 +1,111 @@
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.Mac;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class Encryptor {
 
-	Mac hmac;
+	public String algorithm = "HmacMD5";
+	private SecretKey secretkey = null;
+	private Mac hmac = null;
+	private File root = null;
 	
-	public Encryptor(String exceptions, String secretKey) throws NoSuchAlgorithmException {
-			this.hmac = Mac.getInstance("HmacMD5");
-		
+	public Encryptor(String exceptions, File root, String password) {
+		this.root = root;
+		System.out.println(root.getAbsolutePath());
+		this.createKeyFromString(password);	
+
+		try {
+			this.hmac = Mac.getInstance(this.algorithm);
+			this.hmac.init(secretkey); 
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error: No algorithm with the name" + algorithm + "found!");
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			System.out.println("Error: The key provided is not valid!");
+			e.printStackTrace();
+		}
+	}
+	
+	private void createKeyFromString(String password) {
+		byte[] keydata = password.getBytes();
+		//secretkey = new SecretKeySpec(keydata, "HmacMD5");
+		try {
+			secretkey = KeyGenerator.getInstance(algorithm).generateKey();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void index() {
-		
+		this.index(root);
 	}
 	
-	public void returnKey() {
+	private void index(File dir) {
+		File[] children = dir.listFiles();
+		for (String s : dir.list()) {
+			System.out.println(s);
+		}
+		PrintWriter out = null;
+		
+		try {
+			File indexFile = new File(dir, ".cipherIndex.txt");
+			out = new PrintWriter(new FileWriter(indexFile));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		for (File file : children) {
+			if (file.isHidden()) {
+				System.out.println("hidden file: " + file.getName());
+				continue;
+			}else if (file.isDirectory()) {
+				this.index(file);
+			}
+			System.out.println(file.getName());
+			byte[] ciphertext = this.encrypt(file);
+			String line = new String(ciphertext);
+			out.println(line);
+		}
+		out.close();
+	}
+	
+	private byte[] encrypt (File f) {		
+		byte[] ciphertext = null;
+		
+		try {
+			int filesize = (int) f.length();
+			byte[] data = new byte[filesize];
+			DataInputStream in = new DataInputStream (new FileInputStream(f));
+			in.readFully(data);
+			in.close();
+			
+			ciphertext = hmac.doFinal(data);
+			System.out.println(ciphertext);
+		} catch (FileNotFoundException e) {
+			System.out.println("The File provided does not exist!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("There was an eror reading the file!");
+			e.printStackTrace();
+		}
+		
+		return ciphertext;
+	}
+	
+	public void analyze() {
 		
 	}
 	
--- security/SecurityAssignment1/src/Heimdall.java
+++ security/SecurityAssignment1/src/Heimdall.java
@@ -1,9 +1,24 @@
+import java.io.File;
+
 
 public class Heimdall {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String exceptionsFile = args[0];
+		String exceptions;
+		try {
+			exceptions = args[0];
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("No Exceptions");
+			exceptions = "";
+		}
+		File root = new File("/Users/mars/Desktop/bla");
+		String password = "testpassword";
+		
+		Encryptor encryptor = new Encryptor(exceptions, root, password);
+		encryptor.index();
+		
+		System.out.println("finished!");
 	}
 
 }
