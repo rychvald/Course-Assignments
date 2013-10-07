d16cd0298ce262ad35e420241b83312a65abc0df
rychvald <marcel.stolz@students.unibe.ch>
2013-10-08 01:38:48 +0200

    implemented some stuff on analyzing

--- security/SecurityAssignment1/src/Encryptor.java
+++ security/SecurityAssignment1/src/Encryptor.java
@@ -1,12 +1,17 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
@@ -16,24 +21,15 @@ public class Encryptor {
 
 	public String algorithm = "HmacMD5";
 	private SecretKey secretkey = null;
-	private Mac hmac = null;
 	private File root = null;
+	private String sep = ",";
+	private String cipherFile = ".cipherIndex.txt";
 	
 	public Encryptor(String exceptions, File root, String password) {
 		this.root = root;
 		System.out.println(root.getAbsolutePath());
 		this.createKeyFromString(password);	
 
-		try {
-			this.hmac = Mac.getInstance(this.algorithm);
-			this.hmac.init(secretkey); 
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("Error: No algorithm with the name" + algorithm + "found!");
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			System.out.println("Error: The key provided is not valid!");
-			e.printStackTrace();
-		}
 	}
 	
 	private void createKeyFromString(String password) {
@@ -59,7 +55,7 @@ public class Encryptor {
 		PrintWriter out = null;
 		
 		try {
-			File indexFile = new File(dir, ".cipherIndex.txt");
+			File indexFile = new File(dir, this.cipherFile);
 			out = new PrintWriter(new FileWriter(indexFile));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -76,7 +72,7 @@ public class Encryptor {
 			}
 			System.out.println(file.getName());
 			byte[] ciphertext = this.encrypt(file);
-			String line = new String(ciphertext);
+			String line = file.getName() + this.sep + new String(ciphertext);
 			out.println(line);
 		}
 		out.close();
@@ -84,6 +80,7 @@ public class Encryptor {
 	
 	private byte[] encrypt (File f) {		
 		byte[] ciphertext = null;
+		Mac hmac = null;
 		
 		try {
 			int filesize = (int) f.length();
@@ -91,14 +88,21 @@ public class Encryptor {
 			DataInputStream in = new DataInputStream (new FileInputStream(f));
 			in.readFully(data);
 			in.close();
-			
+			hmac = Mac.getInstance(this.algorithm);
+			hmac.init(this.secretkey);
 			ciphertext = hmac.doFinal(data);
-			System.out.println(ciphertext);
+			System.out.print(ciphertext);
 		} catch (FileNotFoundException e) {
 			System.out.println("The File provided does not exist!");
 			e.printStackTrace();
 		} catch (IOException e) {
 			System.out.println("There was an eror reading the file!");
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error: No algorithm with the name" + algorithm + "found!");
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			System.out.println("Error: The key provided is not valid!");
 			e.printStackTrace();
 		}
 		
@@ -106,7 +110,73 @@ public class Encryptor {
 	}
 	
 	public void analyze() {
+		this.analyze(root);
+	}
+	
+	private void analyze(File dir) {
+		Set<String> children = new HashSet<String>(Arrays.asList(dir.list()));
+		Set<String> previousChildren = this.previousChildren(dir);
+		Set<String> modified = new HashSet<String>(), deleted, newFiles = new HashSet<String>();
 		
+		Set<String> previousCiphers = this.previousCiphers(dir);
+		
+		for ( String child : children) {
+			if ( previousChildren.contains(child)) {
+				previousChildren.remove(child);
+			} else if ( !previousChildren.contains(child) ) {
+				newFiles.add(child);
+			}
+		} //now, previousChildren contains but the deleted files and newFiles all the new files
+		
+		deleted = new HashSet<String>(previousChildren);
+		
+		
+		
+	}
+	
+	private HashSet<String> previousChildren (File dir) {
+		HashSet<String> previousChildren = new HashSet<String>();
+		String cipherContents = this.cipherContents(dir);
+		
+		for ( String line : cipherContents.split("\n") ) {
+			String filename = line.split(this.sep)[0];
+			previousChildren.add(filename);
+		}
+		
+		return previousChildren;
+	}
+	
+	private HashSet<String> previousCiphers (File dir) {
+		HashSet<String> previousCiphers = new HashSet<String>();
+		String cipherContents = this.cipherContents(dir);
+		
+		for ( String line : cipherContents.split("\n") ) {
+			String filename = line.split(this.sep)[1];
+			previousCiphers.add(filename);
+		}
+		
+		return previousCiphers;
+	}
+	
+	private String cipherContents (File dir) {
+		String filename = null;
+		String lines = null;
+		
+		try {
+			filename = dir.getCanonicalPath() + File.separator + this.cipherFile;
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String line;
+			while((line = in.readLine()) != null) {
+				lines = new String(lines + line);
+			}
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return lines;
 	}
 	
 }
