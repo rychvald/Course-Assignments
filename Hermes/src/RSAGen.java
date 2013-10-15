bc8aa2afbe9d2cd1dd65988e0dc367550c25d6e2
rychvald <marcel.stolz@students.unibe.ch>
2013-10-15 23:46:07 +0200

    Hermes added

--- Hermes/src/RSAGen.java
+++ Hermes/src/RSAGen.java
@@ -1,8 +1,25 @@
+import java.util.Random;
+
 
 public class RSAGen {
 
+	private Long publicKey, privateKey;
+	
 	public RSAGen() {
 		
+	}
+	
+	public void init() {
+		Random random = new Random();
+		random.
+	}
+	
+	public Long getPublicKey() {
+		return null;
+	}
+	
+	private Long generatePrime() {
+		return null;
 	}
 	
 }
