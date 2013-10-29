9a11112089cd442fe1198c00dbeec3dc18713d68
rychvald <marcel.stolz@students.unibe.ch>
2013-10-30 00:57:01 +0100

    finished with implementation - working copy

--- LDAP/src/Helper.java
+++ LDAP/src/Helper.java
@@ -1,6 +1,5 @@
 import javax.naming.*;
 import javax.naming.directory.*;
-import javax.naming.ldap.*;
 
 import java.util.Hashtable;
 
@@ -10,48 +9,93 @@ public class Helper {
 	DirContext dirContext;
 	
 	public Helper() {
-		this("edu.security" , "ldap://198.211.110.141:389/ou=students,dc=security,dc=edu");
+		this("ldap://198.211.110.141:389/ou=students,dc=security,dc=edu" , "cn=admin,dc=security,dc=edu" , "security");
 	}
 	
-	public Helper(String context , String server) {
+	public Helper(String provider , String login , String password) {
+		provider = "ldap://" + provider;
 		Hashtable<String , String> environment = new Hashtable<String , String>();
 		environment.put(Context.INITIAL_CONTEXT_FACTORY , "com.sun.jndi.ldap.LdapCtxFactory");
-		environment.put(Context.PROVIDER_URL , server);
+		environment.put(Context.PROVIDER_URL , provider);
 		environment.put(Context.SECURITY_AUTHENTICATION , "simple");
-		environment.put(Context.SECURITY_PRINCIPAL , "cn=admin,dc=security,dc=edu");
-		environment.put(Context.SECURITY_CREDENTIALS , "security");
+		environment.put(Context.SECURITY_PRINCIPAL , login);
+		environment.put(Context.SECURITY_CREDENTIALS , password);
 		try {
 			this.dirContext = new InitialDirContext(environment);
 		} catch (NamingException e) {
 			System.out.println("Error: Could not create Context. Name not valid.");
 			e.printStackTrace();
 		}
-		System.out.println("Successfully connected to LDAP Server :)");
+		System.out.println("Successfully connected to LDAP Server " + provider);
 	}
 	
 	public void list() {
 		try {
-			System.out.println(this.dirContext.getNameInNamespace());
-			Attributes a = this.dirContext.getAttributes("cn=John Doe");
-			NamingEnumeration<NameClassPair> e = this.dirContext.list("");
-			while (e.hasMore()) {
-				System.out.println(e.next().ge);
+			System.out.println("Listing contents of " + this.dirContext.getNameInNamespace());
+			NamingEnumeration<NameClassPair> nameEnum = this.dirContext.list("");
+			while(nameEnum.hasMore()) {
+				System.out.println(nameEnum.next().getName());
 			}
-			System.out.println(a.toString());
-			//NamingEnumeration<String> nameEnum = this.dirContext.list("");
-			//System.out.println(nameEnum.nextElement());
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+	}
+	
+	public void list(String object) {
+		NamingEnumeration<?> nameEnum;
+		try {
+			nameEnum = this.dirContext.getAttributes(object).getAll();
+			System.out.println("Listing attributes of " + object + "," + this.dirContext.getNameInNamespace());
+			while(nameEnum.hasMore()) {
+				System.out.println(nameEnum.next().toString());
+			}
+		} catch (NamingException e) {
+			System.out.println("Error: no such object!");
+			//e.printStackTrace();
+		}
+	}
+	
+	public void changeContext(String object) {
+		try {
+			this.dirContext = (DirContext)this.dirContext.lookup(object);
+			System.out.println("New context: " + this.dirContext.getNameInNamespace());
+		} catch (NamingException e) {
+			System.out.println("Error: no such object!");
+			//e.printStackTrace();
+		}
+	}
+	
+	public void add(String[] strings) {
+		if(strings[0].equals(""))
+			System.out.println("Error: You need to provide a compund name!");
+		if(strings[1].equals(""))
+			System.out.println("Error: You need to provide a surname!");
+		BasicAttributes attr = new BasicAttributes();
+		attr.put("objectClass", "person");
+		String[] attrIDs = {"cn" , "sn" , "userPassword" , "telephoneNumber" , "seeAlso" , "description"};
+		String cn = attrIDs[0] + "=" + strings[0];
+		for(int i=1 ; i<6 ; i++) {
+			if(strings[i].equals(""))
+				continue;
+			else
+				attr.put(attrIDs[i], strings[i]);
+		}
+		try {
+			this.dirContext.bind(cn , null , attr);
+		} catch (NamingException e) {
+			System.out.println("Error: could not add entry!");
 			e.printStackTrace();
 		}
 	}
 	
-	public void add(String newEntry) {
-		
-	}
-	
-	public void remove(String entry) {
-		
+	public void remove(String object) {
+		try {
+			this.dirContext.unbind(object);
+		} catch (NamingException e) {
+			System.out.println("Error: could not remove object " + object + "!");
+			//e.printStackTrace();
+		}
 	}
 	
 }
--- LDAP/src/LDAP.java
+++ LDAP/src/LDAP.java
@@ -1,7 +1,8 @@
-import javax.naming.NamingException;
-import javax.naming.ldap.InitialLdapContext;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+import javax.naming.NamingException;
 
 
 public class LDAP {
@@ -9,8 +10,86 @@ public class LDAP {
 	static String host = "98.211.110.141" , cn = "admin" , dc = "security" , dc2 = "edu";
 	
 	public static void main(String[] args) throws NamingException {
-		Helper helper = new Helper();
-		helper.list();
+		String provider=null , login=null , password=null;
+		if(args.length == 0) {
+			System.out.println("Please provide an argument: \nLDAP server-address [DN(login),password]");
+			System.exit(0);
+		} else if (args.length == 1) {
+			provider = args[0];
+			login = askLogin();
+			password= askPass();
+		} else if (args.length == 2) {
+			provider = args[0];
+			login = args[1];
+			password= askPass();
+		} else {
+			provider = args[0];
+			login = args[1];
+			password= args[2];
+		}
+		Helper helper = new Helper(provider,login,password);
+		String input = null;
+		while(!((input = prompt()).equals("exit"))) {
+			String[] string = input.split(" ",2);
+			int length = string.length;
+			if (length == 0) {
+				continue;
+			} else if (string[0].equals("ls") && (length == 1)) {
+				helper.list();
+			} else if (string[0].equals("ls") && (length == 2)) {
+				helper.list(string[1]);
+			} else if (string[0].equals("cd") && (length == 2)) {
+				helper.changeContext(string[1]);
+			} else if (string[0].equals("add")) {
+				add(helper);
+			} else if (string[0].equals("rm") && (length == 2)) {
+				helper.remove(string[1]);
+			} else {
+				System.out.println("Wrong argument!");
+				System.out.println("Use: [ list[ subcontext] | remove thisentry | add thisentry]");
+			}
+		}
+		helper.dirContext.close();
+		System.out.println("Bye bye!");
+	}
+	
+	static String askLogin() {
+		return ask("Please enter the login user: ");
+	}
+	
+	static String askPass() {
+		return ask("Please enter the password: ");
+	}
+	
+	static String prompt() {
+		return ask(" > ");
+	}
+	
+	static String ask(String question) {
+		String pw = null;
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			System.out.print(question);
+			try {
+				pw = console.readLine();
+			} catch (IOException e) {
+				System.out.println("Problem reading line!");
+				e.printStackTrace();
+			}
+		} while (pw == null);
+		
+		return pw;
+	}
+	
+	static void add(Helper helper) {
+		String[] strings = new String[6];
+		strings[0] = ask("Name of the new User: ");
+		strings[1] = ask("Surname of the new User: ");
+		strings[2] = ask("Password of the new User (optional): ");
+		strings[3] = ask("Telephone of the new User (optional): ");
+		strings[4] = ask("See also (optional): ");
+		strings[5] = ask("Description (optional): ");
+		helper.add(strings);
 	}
 
 }
