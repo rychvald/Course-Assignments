07cc8518b134acac6031774cbac849ad8ad90f78
rychvald <marcel.stolz@students.unibe.ch>
2013-11-05 17:36:53 +0100

    Mail Forwarding working with UniBe server

--- SMTP/Relayer.py
+++ SMTP/Relayer.py
@@ -3,19 +3,21 @@ import smtpd
 import smtplib
 
 class Relayer(smtpd.SMTPServer):
-	def process_message(self , peer , mailfrom , rcpttos, data):
+	def process_message(self , peer , mailfrom , recipients, data):
 		print 'Receiving message from:', peer
 		print 'Message addressed from:', mailfrom
-		print 'Message addressed to  :', rcpttos
+		print 'Message addressed to  :', recipients
 		print 'Message length        :', len(data)
 		print 'Message content       :', data
-		self.send_message(self , peer , mailfrom , recipients , data)
+		self.send_message(mailfrom , recipients , data)
 		return
 	
-	def send_message(self , peer , mailfrom , recipients , data):
-		self.smtplib.SMTP('gmail.com' , )
+	def send_message(self , mailfrom , recipients , data):
+		server = smtplib.SMTP('mail.campus.unibe.ch' , 587)
+		server.starttls()
+		server.login("ms07w881" , "n4schl3daN0u")
 		try:
-			server.sendmail()
+			server.sendmail(mailfrom , recipients , data)
 		finally:
 			server.quit()
 		return		
