be4d1b21a5e0989c7af9002708a4f85248f15d4d
rychvald <marcel.stolz@students.unibe.ch>
2013-11-06 01:28:30 +0100

    functioning copy of SMTP app added

--- SMTP/Relayer.py
+++ SMTP/Relayer.py
@@ -1,6 +1,7 @@
 #Class Relayer.py
 import smtpd
 import smtplib
+import getpass
 
 class Relayer(smtpd.SMTPServer):
 	def process_message(self , peer , mailfrom , recipients, data):
@@ -9,24 +10,36 @@ class Relayer(smtpd.SMTPServer):
 		print 'Message addressed to  :', recipients
 		print 'Message length        :', len(data)
 		print 'Message content       :', data
-		newData = self.filter_message(data)
+		print self.dropfilter(data)
+		if self.dropfilter(data)!=-1: return
+		newData = self.textfilter(data)
 		self.send_message(mailfrom , recipients , newData)
 		return
 	
 	def send_message(self , mailfrom , recipients , data):
 		server = smtplib.SMTP('mail.campus.unibe.ch' , 587)
 		server.starttls()
-		server.login("ms07w881" , "n4schl3daN0u")
+		password=getpass.getpass("enter a password: ")
+		server.login("ms07w881" , password)
 		try:
 			server.sendmail(mailfrom , recipients , data)
 		finally:
 			server.quit()
 		return	
 
-	def add_filter(self , filters):
+	def add_textfilter(self , filters):
 		self.filters = filters
 		return
+	def add_dropfilter(self , filters):
+		self.dropfilters = filters
+		return
 
-	def filter_message(self , message):
-		
+	def textfilter(self , message):
+		for textfilter in self.filters:
+			message=message.replace(textfilter , '')
 		return message
+
+	def dropfilter(self , message):
+		for dropfilter in self.dropfilters:
+			retVal=message.find(dropfilter)
+		return retVal
