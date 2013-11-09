613fdb8c1903b45e04e7b857c1f24f1e9be3227f
rychvald <marcel.stolz@students.unibe.ch>
2013-11-09 17:46:31 +0100

    added smtp Antivir project

---
+++ SMTPAntiVir/Relayer.py
@@ -0,0 +1,45 @@
+#Class Relayer.py
+import smtpd
+import smtplib
+import getpass
+
+class Relayer(smtpd.SMTPServer):
+	def process_message(self , peer , mailfrom , recipients, data):
+		print 'Receiving message from:', peer
+		print 'Message addressed from:', mailfrom
+		print 'Message addressed to  :', recipients
+		print 'Message length        :', len(data)
+		print 'Message content       :', data
+		print self.dropfilter(data)
+		if self.dropfilter(data)!=-1: return
+		newData = self.textfilter(data)
+		self.send_message(mailfrom , recipients , newData)
+		return
+	
+	def send_message(self , mailfrom , recipients , data):
+		server = smtplib.SMTP('mail.campus.unibe.ch' , 587)
+		server.starttls()
+		password=getpass.getpass("enter a password: ")
+		server.login("ms07w881" , password)
+		try:
+			server.sendmail(mailfrom , recipients , data)
+		finally:
+			server.quit()
+		return	
+
+	def add_textfilter(self , filters):
+		self.filters = filters
+		return
+	def add_dropfilter(self , filters):
+		self.dropfilters = filters
+		return
+
+	def textfilter(self , message):
+		for textfilter in self.filters:
+			message=message.replace(textfilter , '')
+		return message
+
+	def dropfilter(self , message):
+		for dropfilter in self.dropfilters:
+			retVal=message.find(dropfilter)
+		return retVal
