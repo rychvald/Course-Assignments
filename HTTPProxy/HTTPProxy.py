afa7a8aadcc819b44efbfae5714b05860796dbd7
rychvald <marcel.stolz@students.unibe.ch>
2013-11-24 22:30:03 +0100

    HTTPProxy working nicely :)

--- HTTPProxy/HTTPProxy.py
+++ HTTPProxy/HTTPProxy.py
@@ -2,24 +2,37 @@
 import os
 import urllib
 import urlparse
+import fnmatch
 import SimpleHTTPServer
 
 class HTTPProxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+	def getfilters(self , filterfile):
+		with open(filterfile) as f:
+			filters = f.readlines()
+		newfilters=[]
+		for dropfilter in filters:
+			newfilters.append(dropfilter.replace('\n' , ''))
+		return newfilters
+
 	def do_GET(self):
 		print self.path
-		self.copyfile(urllib.urlopen(self.path), self.wfile)
-		self.blockedurl()
+		if self.blocked_path(self.getfilters("blacklist.txt")):
+			self.blockedurl()
+		else:
+			self.copyfile(urllib.urlopen(self.path), self.wfile)
 
 	def blockedurl(self):
 		print "Returning blocked URL"
 		retVal = "HTTP/1.1 403 Forbidden\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\nContent blocked by proxy\r\n"
 		self.wfile.write(retVal)
 
-	def blocked_path(self):
-		return
-
-	def load_blacklist(self , blacklist):
-		self.blacklist = blacklist
-		print self.blacklist
-		return
+	def blocked_path(self , blacklist):
+		servername = urlparse.urlparse(self.path)
+		servername = servername.netloc
+		print servername
+		retVal = 0
+		for blackstring in blacklist :
+			print blackstring
+			if fnmatch.fnmatch(servername , blackstring): retVal = 1
+		return retVal
