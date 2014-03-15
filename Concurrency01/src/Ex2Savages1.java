d79410112e37c8b23074d29e7ed6184b8c70dea9
rychvald <marcel.stolz@students.unibe.ch>
2014-03-15 17:54:00 +0100

    Ex2.1 working

--- Concurrency01/src/Ex2Savages1.java
+++ Concurrency01/src/Ex2Savages1.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class Ex2Savages1 {
 
 	int capacity;
-	int pot;
+	volatile int pot;
 	Cook cook;
 	Set<Savage> savages;
 	static Ex2Savages1 tribe;
@@ -22,7 +22,6 @@ public class Ex2Savages1 {
 	public Ex2Savages1(int capacity, int savageNumber) {
 		this.capacity = capacity;
 		this.cook = new Cook();
-		this.cook.refillRequest();
 		this.savages = new HashSet<Savage>();
 		while(savageNumber > 0) {
 			this.savages.add(new Savage());
@@ -31,6 +30,7 @@ public class Ex2Savages1 {
 	}
 	
 	public void enjoyYourMeal() {
+		this.cook.start();
 		for(Savage savage : this.savages) {
 			savage.start();
 		}
@@ -56,25 +56,34 @@ public class Ex2Savages1 {
 		private void hungry() {
 			System.out.println("Savage is hungry, but pot is empty!");
 			cook.refillRequest();
+			yield();
 			this.eat();
 		}
 	}
 	
 	public class Cook extends Thread {
+		private boolean refill = true;
+		
 		public void run() {
-			this.refillPot();
-			
+			while(true) {
+				if(this.refill)
+					this.refillPot();
+				yield();
+			}
 		}
 		
 		private void refillPot() {
-			System.out.println("Pot seems to be empty: "+pot);
-			assert pot == 0;
-			pot = capacity;
-			System.out.println("Cook has refilled pot: "+pot);
+			synchronized(tribe) {	
+				System.out.println("Pot seems to be empty: "+pot);
+				assert pot == 0;
+				pot = capacity;
+				this.refill = false;
+				System.out.println("Cook has refilled pot: "+pot);
+			}
 		}
 		
-		public void refillRequest() {
-			this.refillPot();
+		public synchronized void refillRequest() {
+			this.refill = true;
 		}
 	}
 }
